5e6c7de7448b4741598657310c4ff46f4bda1f44
Nidhin <myofficialskknidhin@gmail.com>
2025-05-10 21:32:57 +0530

    Adding example for single responsibility principle

---
+++ solid_design_principles/single_responsibility_principle/journal_single_responsibility_principle.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+#include "journal_single_responsibility_principle.h"
+
---
+++ solid_design_principles/single_responsibility_principle/journal_single_responsibility_principle.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+class journal {
+public:
+	string title;
+	vector<string> entries;
+};
