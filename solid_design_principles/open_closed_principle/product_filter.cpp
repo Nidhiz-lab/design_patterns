cfb9f7702aaa4c0ef29d019e989bd1941593df9c
Nidhin <myofficialskknidhin@gmail.com>
2025-05-24 17:31:32 +0700

    [may 24] Open close principle example using filter mechanism

--- solid_design_principles/open_closed_principle/product_filter.cpp
+++ solid_design_principles/open_closed_principle/product_filter.cpp
@@ -13,3 +13,55 @@ vector<Product*> ProductFilter::by_color(vector<Product*> items, Color color)
 	}
 	return result;
 }
+
+vector<Product*> ProductFilter::by_size(vector<Product*> items, Size size)
+{
+	vector<Product*> result;
+	for (auto& i : items)
+	{
+		if (i->size == size)
+		{
+			result.push_back(i);
+		}
+	}
+	return result;
+}
+
+vector<Product*> ProductFilter::by_color_and_size(vector<Product*> items, Color color, Size size)
+{
+	vector<Product*> result;
+	for (auto& i : items)
+	{
+		if ((i->size == size) && (i->color == color))
+		{
+			result.push_back(i);
+		}
+	}
+	return result;
+}
+
+
+vector<Product*> BetterFilter::filter(vector<Product*> items, Specification<Product>& spec)
+{
+	vector<Product*> result;
+	for (auto& item : items)
+	{
+		if (spec.is_satisfied(item))
+		{
+			result.push_back(item);
+		}
+	}
+	return result;
+}
+
+bool ColorSpecification::is_satisfied(Product* item)
+{
+	return item->color == this->color;
+
+}
+
+bool SizeSpecification::is_satisfied(Product* item)
+{
+	return item->size == this->size;
+
+}
--- solid_design_principles/open_closed_principle/product_filter.hpp
+++ solid_design_principles/open_closed_principle/product_filter.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <string>
 #include <vector>
+#include <iostream>
 using namespace std;
 enum class Color {red, green, blue};
 enum class Size { small, medium, large };
@@ -18,5 +19,65 @@ class ProductFilter {
 public:
 	ProductFilter(){ }
 	vector<Product*> by_color(vector<Product*> items, Color color);
+	vector<Product*> by_size(vector<Product*> items, Size size);
+	vector<Product*> by_color_and_size(vector<Product*> items, Color color, Size size);
 
 };
+
+
+template <typename T> class Specification {
+public:
+	Specification() {}
+	virtual bool is_satisfied(T* item) = 0;
+};
+
+template <typename T> class Filter {
+public:
+	Filter(){}
+	virtual vector<T*> filter(vector<T*> items, Specification<T>& spec) = 0;
+
+};
+
+class BetterFilter : public Filter<Product> 
+{
+public:
+	
+	BetterFilter(){}
+	vector<Product*> filter(vector<Product*> items, Specification<Product>& spec) override;
+};
+
+class ColorSpecification : public Specification<Product> {
+public:
+	Color color;
+	ColorSpecification(Color color): color(color){}
+
+	bool is_satisfied(Product* item) override;
+
+};
+
+class SizeSpecification : public Specification<Product> {
+public:
+	Size size;
+	SizeSpecification(Size size) : size(size) {}
+
+	bool is_satisfied(Product* item) override;
+
+};
+template<typename T>
+class AndSpecification : public Specification<T>
+{
+public:
+	Specification<T>& first;
+	Specification<T>& second;
+	AndSpecification(Specification<T>& first, Specification<T>& second) : first(first), second(second){}
+	bool is_satisfied(T* item) override
+	{
+		return this->first.is_satisfied(item) && this->second.is_satisfied(item);
+	}
+};
+
+template<typename T>
+AndSpecification<T> operator&&(Specification<T>&& first, Specification<T>&& second)
+{
+	return AndSpecification<T>(first, second);
+}
